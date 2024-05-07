dfe3c5af66dae49b16693bfef3a8e25479ed16d5
camilatavera <78244053+camilatavera@users.noreply.github.com>
2024-05-07 19:21:15 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './componentes/login/login.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
+import { ChatComponent } from './componentes/chat/chat.component';
 
 
 export const routes: Routes = [
@@ -28,7 +29,15 @@ export const routes: Routes = [
   { 
     path:'registro', 
     component: RegistroComponent 
-  }
+  },
+  {
+    path: 'juegos', 
+    loadChildren: () => import('./juegos/juegos.module').then(m => m.JuegosModule)
+  },
+  { 
+    path:'chat', 
+    component: ChatComponent 
+  },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
-
 import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
-import { RouterModule } from '@angular/router'; // importo RouterModule
+import { HeaderComponent } from './componentes/header/header.component';
 
 
+import { FormsModule } from '@angular/forms'; 
 
-
-import { FormsModule } from '@angular/forms';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { ChatComponent } from './componentes/chat/chat.component';
+
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 
 @NgModule({
@@ -26,23 +28,25 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     LoginComponent,
     HomeComponent,
     RegistroComponent,
-    QuienSoyComponent
+    QuienSoyComponent,
+    ChatComponent,
+    HeaderComponent,
 
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    RouterModule, // Agrego RouterModule
-    AppRoutingModule,// importo rutas definidas aca
-
-    
+    FormsModule,  //importamos el modulo de formularios para poder usar ngModel
     provideFirebaseApp(() => initializeApp({"projectId":"sala-de-juegos-b232b","appId":"1:1028974691964:web:e73dbb99104fb8324c2bf9","storageBucket":"sala-de-juegos-b232b.appspot.com","apiKey":"AIzaSyCfxp3SKOfMxhfm1XMdBUSIypiuxKxKmS0","authDomain":"sala-de-juegos-b232b.firebaseapp.com","messagingSenderId":"1028974691964"})),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
-    
+    provideFirestore(() => getFirestore()),
+
+    BrowserAnimationsModule, //  animaciones de ngx-toastr
+    ToastrModule.forRoot(), // config global de ngx-toastr 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule { } 
